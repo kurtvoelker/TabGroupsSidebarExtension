08d5ad17a6cd90158ec48e2428584fafbd2b7810
kurtvoelker <kurt.voelker@gmail.com>
2026-06-20 10:33:43 -0400

    update store URL

--- permissions.js
+++ permissions.js
@@ -12,8 +12,8 @@ const FEATURES = {
 const FREE_WORKSPACE_LIMIT = 3; // eslint-disable-line no-unused-vars
 
 const LS_STORE_ID       = 341342;
-const LS_URL_ANNUAL     = 'https://tabgroups.lemonsqueezy.com/checkout/buy/d68f5603-c188-4b51-a7f6-fa8140a53eff';
-const LS_URL_LIFETIME   = 'https://tabgroups.lemonsqueezy.com/checkout/buy/c3af4842-c2c8-41c1-8743-6550f86423a8';
+const LS_URL_ANNUAL     = 'https://kvextensions.lemonsqueezy.com/checkout/buy/d68f5603-c188-4b51-a7f6-fa8140a53eff';
+const LS_URL_LIFETIME   = 'https://kvextensions.lemonsqueezy.com/checkout/buy/c3af4842-c2c8-41c1-8743-6550f86423a8';
 
 const LS_ACTIVATE_URL   = 'https://api.lemonsqueezy.com/v1/licenses/activate';
 const LS_VALIDATE_URL   = 'https://api.lemonsqueezy.com/v1/licenses/validate';
